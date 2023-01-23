fd38511a26775f8425b077a6f1cb38bbfa8c300b
Nizami Sevindi <nsevindi87@gmail.com>
2023-01-23 10:43:10 +0100

    learning-React-testing

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,39 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+/*
+* getBy... getAllBy...
+* queryBy...  queryAllBy...
+* findBy... findAllBy...
+*/
+
+/* 
+  !!Queries Accesible to Everyone
+?  ======= getByRole
+
+
+
+
+
+?  ======= getByLabelText   (label)
+?  ======= getByPlaceholderText
+?  ======= getByText
+?  ======= getByDisplayValue
+
+  !!Semantic Queries
+?  ======= getByAltText (img, area, input)
+?  ======= getByTitle
+
+  !!Test IDs
+?  ======= getByTestId
+*/
+
+/*
+!!TextMatch 
+* getByText("Hello world!")
+* getByText("ello wor", {exact: false})
+* getByText(/world/i)
+* getByText(/Hello W?oRld/i)  ==> It`s not sure whether W should be.
+* There is enother option to choose text. With Funtion
+*/
